0061f0350d9b796904faea522bc30fcf6ab02a97
lucas.sousa <lucas.sousa@kasco.ai>
2026-01-02 15:33:26 -0300

    Fix next index search returning wrong value

--- downloader.py
+++ downloader.py
@@ -1,25 +1,55 @@
 import os
+import subprocess
 
 #print(os.listdir('I:\SteamLibrary\steamapps\common\My Winter Car\Radio'))
 
 nbr = []
-nextIndex = 1
 
-for files in os.listdir('I:\SteamLibrary\steamapps\common\My Winter Car\Radio'):
-    if(files[5:-4] == '' or files[0:5] != "track"):
-        continue
-    nbr.append(int(files[5:-4]))
-    
-nbr.sort()
-#print(nbr)
 
-for i in nbr:
-    if (nextIndex < i):
-        nextIndex = i        
-        break
-    nextIndex = i + 1
-print(nextIndex)
+mwcPath = 'I:\SteamLibrary\steamapps\common\My Winter Car\Radio'
 
-    # if(nbr > lrgnbr):
-    #     lrgnbr = nbr
-    # print(lrgnbr)
+def searchIndex(path):
+    try:
+        os.listdir(path)
+    except:
+        print("Erro")
+    nextIndex = 1
+    for files in os.listdir(path):
+        if(files[5:-4] == '' or files[0:5] != "track"):
+            continue
+        nbr.append(int(files[5:-4]))
+        
+    nbr.sort()
+    #print(nbr)
+
+    for i in nbr:
+        print (f'i = {i}, nextIndex = {nextIndex}')
+        if (nextIndex < i):     
+            return nextIndex
+        nextIndex = i + 1
+    return nextIndex
+
+trackName = "track" + str(searchIndex(mwcPath))
+
+print(f'Resultado da função: {searchIndex(mwcPath)}')
+
+
+audioPath = os.path.join(mwcPath, trackName)
+# print(audioPath)
+
+cmdyt = [
+    "yt-dlp",
+    # "-h"
+    "-x",
+    # "--audio-format vorbis",
+    "-o",
+    audioPath,
+    "https://www.youtube.com/watch?v=TCd6PfxOy0Y"
+]
+
+cmdffm = [
+    "ffmpeg",
+    "-h"
+]
+
+# subprocess.run(cmdyt, check=True)
